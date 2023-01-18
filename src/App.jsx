ef8a2015813be96a9e525eddebe43c7d8135c7ed
BrenoRicardo <breno02.carvalho@gmail.com>
2023-01-17 23:18:37 -0300

    Trabalho

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  redirect,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Home from "./routes/Home.jsx";
+import Filmes from "./routes/Filmes.jsx";
+import Sobre from "./routes/Sobre.jsx";
+import Footer from "./components/Footer.jsx";
+import PostFilme from "./routes/PostFilme.jsx";
+import Cadastro from "./routes/Cadastro.jsx";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
+        <Route path="/Filmes" element={<Filmes />} />
+        <Route path="/Sobre" element={<Sobre />} />
+        <Route path="/PostarFilme" element={<PostFilme />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
